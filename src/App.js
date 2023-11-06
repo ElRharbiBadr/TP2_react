139a9653c80e5967de058a27b5231a223aa80e4d
ElRharbiBadr <stardust12003@gmail.com>
2023-11-06 15:50:19 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+// import Bb from"./comp" //isem dial file comp 
+import './App.css';
+import Comp3 from "./comp3";
+import Comp2 from './comp2';
+import UseRef from './UseRef';
+import Calcul from './comp';
+import Compimg from './tpreact/slider/src/Compimg'
+function App() {
+  
+
+  return (
+    <div className="App">
+{/*   
+    <Comp2/> */}
+    <Compimg/>
+    </div>
+  );
+}
+
+export default App;
